0f29a472ac4aa1fbe070e0b117d440c7ef5a494c
DATA GURU <gyrogodnon@gmail.com>
2026-06-26 15:26:20 +0530

    commit

--- terraform/main.tf
+++ terraform/main.tf
@@ -131,6 +131,15 @@ resource "aws_vpc_endpoint" "bedrock_runtime" {
   private_dns_enabled = true
 }
 
+resource "aws_vpc_endpoint" "cloudwatch_logs" {
+  vpc_id              = aws_vpc.main.id
+  service_name        = "com.amazonaws.${var.aws_region}.logs"
+  vpc_endpoint_type   = "Interface"
+  subnet_ids          = aws_subnet.private[*].id
+  security_group_ids  = [aws_security_group.vpc_endpoints.id]
+  private_dns_enabled = true
+}
+
 resource "aws_security_group" "vpc_endpoints" {
   name   = "${var.project}-vpc-endpoints"
   vpc_id = aws_vpc.main.id
